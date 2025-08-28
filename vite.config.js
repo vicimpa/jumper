c5cb22ca472430964c9c0840449d92fd3b909a6e
PromiSe#### <vicimpa@ya.ru>
2025-08-28 05:30:52 +0300

    Fix build again

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '.',
+  base: './',
   root: './src',
   publicDir: '../public',
   server: { host: true, port: 3000 },
